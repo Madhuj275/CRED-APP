d259aedc5e6ca82e0142e1ede21092b364469578
Madhuj275 <madhuj.2004@yahoo.com>
2025-07-11 01:06:47 +0530

    Saving unstaged changes

--- frontend/src/redux/store.js
+++
@@ -1,7 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import { thunk } from 'redux-thunk';
-import userReducer from './reducers/userReducer';
-
-const store = createStore(userReducer, applyMiddleware(thunk));
-
-export default store;
